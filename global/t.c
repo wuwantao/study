3259c9cb2869dd5a9a0b7fe475b98d90f6848ac7
root <root@localhost.localdomain>
2020-04-13 10:30:34 +0800

    global

---
+++ global/t.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+struct in_addr  in4_addr;
+struct in6_addr in6_addr;
+char str[128];
+int main() 
+{
+	int ret = 0;
+	const char *p = "192.168.1.1/24";
+
+
+	printf("inet_pton: 0x%x\n", in4_addr.s_addr); 
+	ret = inet_pton(AF_INET, p, &in4_addr);
+
+	printf("ret:%d\n", ret);
+	printf("inet_pton: 0x%x\n", in4_addr.s_addr); 
+
+	if (ret != 1) {
+		perror("inet_pton");
+	}
+
+	return 1;
+}
